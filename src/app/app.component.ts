2133312b46814615ac192bc5b6a3c8cd121fdd1d
ulises-n64 <ulises.ra.cal@gmail.com>
2024-02-07 17:46:32 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'todoapp';
+  variable='Var 2';
+  tasks=[
+    'Estudiar Angular',
+    'Estudiar SASS',
+    'Estudiar Pruebas unitarias Con Jasmine, Karma',
+    'Estudiar Patrones de diseño Como:  Singleton'
+  ]
 }
